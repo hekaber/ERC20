495205c478cac8d34dc72d0c58cc6e1954d11d3f
Karl <hkberger@gmail.com>
2023-01-01 23:53:29 +0100

    [TASK] Set deploy configuration

--- src/solidity/hardhat.config.ts
+++ src/solidity/hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + '/config/env/.env' });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.GOERLI_ACCOUNT_KEY]
+    },
+  }
 };
 
 export default config;
