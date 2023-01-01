d7781e5144b1513a95dce96fa6b940011451e587
Karl <hkberger@gmail.com>
2023-01-01 23:37:28 +0100

    [TASK] Add lint basis

---
+++ src/solidity/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "mocha": true,
+        "node": true
+    },
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
