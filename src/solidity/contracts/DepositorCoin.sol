6203bf490b8b138092efa5a5099ed45633bcaa90
Karl <hkberger@gmail.com>
2023-01-03 21:47:25 +0100

    [TASK] Implement burn and mint for coins

---
+++ src/solidity/contracts/DepositorCoin.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+import { ERC20 } from "./ERC20.sol";
+
+
+contract DepositorCoin is ERC20 {
+
+    address public owner;
+
+    constructor() ERC20("DepositorCoin", "DPC") {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(to == owner, "DPC: only owner can mint");
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        require(from == owner, "DPC: only owner can burn");
+        _burn(from, amount);
+    }
+}
---
+++ src/solidity/contracts/StableCoin.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+import { ERC20 } from "./ERC20.sol";
+import { DepositorCoin } from "./DepositorCoin.sol";
+
+
+contract StableCoin is ERC20 {
+
+    DepositorCoin public depositorCoin;
+    uint256 public feeRatePercentage;
+    uint256 private constant ETH_IN_USD_PRICE = 2000;
+
+    constructor(uint256 _feeRatePercentage) ERC20("StableCoin", "STC") {
+        feeRatePercentage = _feeRatePercentage;
+    }
+
+    function mint() external payable {
+
+        uint256 fee = _getFee(msg.value);
+        uint256 remainingEth = msg.value - fee;
+
+        uint256 mintStableCoinAmount = msg.value * ETH_IN_USD_PRICE;
+        _mint(msg.sender, mintStableCoinAmount);
+    }
+
+    function burn(uint256 burnStableCoinAmount) external {
+
+        _burn(msg.sender, burnStableCoinAmount);
+
+        uint256 refundingEth = burnStableCoinAmount / ETH_IN_USD_PRICE;
+        uint256 fee = _getFee(refundingEth);
+        uint256 remainingRefundingEth = refundingEth - fee;
+
+        (bool success,) = msg.sender.call{value: remainingRefundingEth}("");
+        require(success, "STC: burn transaction failed");
+    }
+
+    function _getFee(uint ethAmount) private view returns (uint256) {
+
+        bool hasDepositors = address(depositorCoin) != address(0) && depositorCoin.totalSupply() > 0;
+
+        if (!hasDepositors) {
+            return 0;
+        }
+        return (feeRatePercentage * ethAmount) / 100;
+    }
+}
