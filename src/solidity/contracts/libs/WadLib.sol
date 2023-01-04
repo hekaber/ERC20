edf28de32aadb5f646c69a43ee11f26559efb7e8
Karl <hkberger@gmail.com>
2023-01-04 16:08:48 +0100

    [TASK] Use Wad type instead of struct

--- src/solidity/contracts/libs/WadLib.sol
+++ src/solidity/contracts/libs/WadLib.sol
@@ -5,23 +5,21 @@ pragma solidity 0.8.17;
 library WadLib {
     uint256 public constant MULTIPLIER = 10e18;
 
-    struct Wad {
-        uint256 value;
+    type Wad is uint256;
+
+    function mulWad(uint256 number, Wad wad) internal pure returns (uint256) {
+        return (number * Wad.unwrap(wad)) / MULTIPLIER;
     }
 
-    function mulWad(uint256 number, Wad memory wad) internal pure returns (uint256) {
-        return (number * wad.value) / MULTIPLIER;
+    function divWad(uint256 number, Wad wad) internal pure returns (uint256) {
+        return (number * MULTIPLIER) / Wad.unwrap(wad);
     }
 
-    function divWad(uint256 number, Wad memory wad) internal pure returns (uint256) {
-        return (number * MULTIPLIER) / wad.value;
-    }
-
-    function fromFraction(uint256 numerator, uint256 denominator) internal pure returns (Wad memory) {
+    function fromFraction(uint256 numerator, uint256 denominator) internal pure returns (Wad) {
         if (numerator == 0) {
-            return Wad(0);
+            return Wad.wrap(0);
         }
 
-        return Wad((numerator * MULTIPLIER) / denominator);
+        return Wad.wrap((numerator * MULTIPLIER) / denominator);
     }
 }
